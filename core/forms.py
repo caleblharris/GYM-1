f97a5a764b7b53115aa3f033451208ec1684cf24
Caleb Harris <44626834+caleblharris@users.noreply.github.com>
2021-05-03 06:44:54 -0700

    commit 3

--- core/forms.py
+++ core/forms.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 
 from bootstrap_modal_forms.forms import BSModalModelForm, BSModalForm
 from bootstrap_modal_forms.mixins import PopRequestMixin, CreateUpdateAjaxMixin
-from .models import Customer
+
 
 class CustomerModelForm(BSModalModelForm):
     class Meta:
