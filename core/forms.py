729aafa207c5311f6052fb6729b6fe9268564dd8
Caleb Harris <44626834+caleblharris@users.noreply.github.com>
2021-05-11 23:34:27 -0700

    e-mail commit

--- core/forms.py
+++ core/forms.py
@@ -17,7 +17,7 @@ class CustomerModelForm(BSModalModelForm):
 class CustomUserCreationForm(PopRequestMixin, CreateUpdateAjaxMixin, UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'password1', 'password2']
+        fields = ['username','email', 'password1', 'password2']
 
 
 class CustomAuthenticationForm(AuthenticationForm):
